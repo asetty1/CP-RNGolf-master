d0e92709ad08e1cd3616e37064e19b8c8a223ae0
Ananya Setty <asetty1@ucsc.edu>
2025-01-29 16:13:40 -0800

    3/4 challenges complete

--- src/Play.js
+++ src/Play.js
@@ -8,6 +8,9 @@ class Play extends Phaser.Scene {
         this.SHOT_VELOCITY_X = 700
         this.SHOT_VELOCITY_Y_MIN = 700
         this.SHOT_VELOCITY_Y_MAX = 1100
+        this.ONEWAY_VELOCITY = 100
+
+        this.shotCount = 0
 
     }
 
@@ -46,6 +49,7 @@ class Play extends Phaser.Scene {
         let wallB = this.physics.add.sprite(0, height /2, 'wall')
         wallB.setX(Phaser.Math.Between(0 + wallB.width/2, width - wallB.width/2))
         wallB.body.setImmovable(true)
+        //this.wallB.body.setVelocityX(100)
 
         this.walls = this.add.group([wallA, wallB])
 
@@ -53,6 +57,7 @@ class Play extends Phaser.Scene {
         this.oneWay = this.physics.add.sprite(0, height /4 * 3, 'oneway')
         this.oneWay.setX(Phaser.Math.Between(0 + this.oneWay.width/2, width - this.oneWay.width /2))
         this.oneWay.body.setImmovable(true)
+        this.oneWay.body.setVelocityX(this.ONEWAY_VELOCITY)
         this.oneWay.body.checkCollision.down = false
 
         // add pointer input
@@ -74,13 +79,35 @@ class Play extends Phaser.Scene {
 
         // ball/one-way collision
         this.physics.add.collider(this.ball, this.oneWay)
+
+        //shot counter text
+        let shotCounter = {
+            fontFamily: 'Courier',
+            fontSize: '28px',
+            backgroundColor: '#F3B141',
+            color: '#843605',
+            align: 'right',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 100
+        }
+
+        this.shotCounterText = this.add.text(width /10, height/20, this.shotCount, shotCounter)
     }
 
     update() {
+        if (this.oneWay.x <= 0 + this.oneWay.width/2) {
+            this.oneWay.body.setVelocityX(this.ONEWAY_VELOCITY)
+        } else if (this.oneWay.x >= width - this.oneWay.width/2) {
+            this.oneWay.body.setVelocityX(-this.ONEWAY_VELOCITY)
+        }
 
     }
 
     reset() {
+        this.shotCount += 1
         this.ball.setVelocity(0, 0)
         this.ball.setX(width /2)
         this.ball.setY(height - height /10)
@@ -91,6 +118,6 @@ CODE CHALLENGE
 Try to implement at least 3/4 of the following features during the remainder of class (hint: each takes roughly 15 or fewer lines of code to implement):
 [X] Add ball reset logic on successful shot
 [X] Improve shot logic by making pointer’s relative x-position shoot the ball in correct x-direction
-[ ] Make one obstacle move left/right and bounce against screen edges
+[X] Make one obstacle move left/right and bounce against screen edges
 [ ] Create and display shot counter, score, and successful shot percentage
 */
